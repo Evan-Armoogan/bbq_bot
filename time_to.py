626e40f5c194a97e7ce1759567d5546dbb3487a3
Evan Armoogan <111323350+Evan-Armoogan@users.noreply.github.com>
2025-10-31 23:49:34 -0400

    Change leafs output string format to include years (#21)

--- time_to.py
+++ time_to.py
@@ -8,3 +8,17 @@ def get_time_to_str(target_datetime: int) -> str:
     seconds = int(seconds)
     duration_str = f"{days} days, {hours} hours, {minutes} minutes, and {seconds} seconds"
     return duration_str
+
+
+def get_time_to_years_str(target_datetime: int) -> str:
+    years, remainder = divmod(target_datetime, 3600 * 24 * 365)
+    days, remainder = divmod(remainder, 3600 * 24)
+    hours, remainder = divmod(remainder, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    years = int(years)
+    days = int(days)
+    hours = int(hours)
+    minutes = int(minutes)
+    seconds = int(seconds)
+    duration_str = f"{years} years, {days} days, {hours} hours, {minutes} minutes, and {seconds} seconds"
+    return duration_str
